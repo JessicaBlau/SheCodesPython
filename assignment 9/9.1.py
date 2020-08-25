360894eb075b36605dc1aaa9b8fe9e5f9c4486c2
JessicaBlau <jblau1993@gmail.com>
2020-08-25 19:52:10 +0300

    "update"

---
+++ assignment 9/9.1.py
@@ -0,0 +1,24 @@
+# Question 1
+
+movie = ['The Notebook', 'Maleficent', 'Batman v Superman', 'Black Swan', 'Gone Girl', 'War of the Worlds',
+         'Just Married']
+actor = ['Rachel McAdams', 'Angelina Jolie', 'Gal Gadot', 'Natalie Portman', 'Rosamund Pike', 'Dakota Fanning',
+         'Brittany Murphy']
+
+lists = [i+" is played by "+j for (i, j) in list(zip(movie, actor))]
+
+print(lists)
+
+# Question 2
+
+movies = dict(zip(movie, actor))
+
+print(movies)
+
+# Question 3
+
+newList = [i+" is played by "+j for (i, j) in movies.items()]
+
+print(newList)
+
+
